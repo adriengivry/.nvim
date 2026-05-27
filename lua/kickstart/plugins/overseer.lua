e5941607311caf3453002b6b67076dca3a930207
Adrien GIVRY <contact@adrien-givry.com>
2026-05-27 15:33:11 -0400

    Updated overseer config to support opts

--- lua/kickstart/plugins/overseer.lua
+++ lua/kickstart/plugins/overseer.lua
@@ -2,10 +2,17 @@ return {
   'stevearc/overseer.nvim',
   ---@module 'overseer'
   ---@type overseer.SetupOpts
-  config = function()
+  opts = {
+    task_list = {
+      -- Default direction. Can be "left", "right", or "bottom"
+      direction = 'bottom',
+    },
+  },
+  config = function(_, opts)
+    require('overseer').setup(opts)
+
     vim.keymap.set('n', '<leader>o', '<cmd>OverseerToggle<CR>')
     vim.keymap.set('n', '<F6>', '<cmd>OverseerRun<CR>')
     vim.keymap.set('n', '<C-/>', '<cmd>OverseerShell<CR>')
   end,
-  opts = {},
 }
