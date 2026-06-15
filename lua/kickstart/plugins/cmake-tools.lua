981a1215585ca6ce7829a043ac13d7204975a4ce
Adrien GIVRY <contact@adrien-givry.com>
2026-06-15 15:53:12 -0400

    Added keybindings to cmake tools

--- lua/kickstart/plugins/cmake-tools.lua
+++ lua/kickstart/plugins/cmake-tools.lua
@@ -29,4 +29,21 @@ return {
       console = 'integratedTerminal',
     },
   },
+  config = function(_, opts)
+    require('cmake-tools').setup(opts)
+
+    vim.keymap.set('n', '<Leader>cg', '<cmd>CMakeGenerate<CR>')
+    vim.keymap.set('n', '<Leader>cb', '<cmd>CMakeBuild<CR>')
+    vim.keymap.set('n', '<Leader>cr', '<cmd>CMakeRun<CR>')
+    vim.keymap.set('n', '<Leader>cd', '<cmd>CMakeDebug<CR>')
+    vim.keymap.set('n', '<Leader>ct', '<cmd>CMakeRunTest<CR>')
+    vim.keymap.set('n', '<Leader>cB', '<cmd>CMakeQuickBuild<CR>')
+    vim.keymap.set('n', '<Leader>cD', '<cmd>CMakeQuickDebug<CR>')
+    vim.keymap.set('n', '<Leader>cR', '<cmd>CMakeQuickRun<CR>')
+    vim.keymap.set('n', '<Leader>cscp', '<cmd>CMakeSelectConfigurePreset<CR>')
+    vim.keymap.set('n', '<Leader>csl', '<cmd>CMakeSelectLaunchTarget<CR>')
+    vim.keymap.set('n', '<Leader>csbp', '<cmd>CMakeSelectBuildPreset<CR>')
+    vim.keymap.set('n', '<Leader>csbt', '<cmd>CMakeSelectBuildPreset<CR>')
+    vim.keymap.set('n', '<Leader>cst', '<cmd>CMakeSelectTestPreset<CR>')
+  end,
 }
